d6c8c15a347e52d09e51a4d2e5037c8176c96652
ChetanGade98 <chetangade465@gmail.com>
2024-12-28 00:12:52 -0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+public static void main (String [] args )
+  {
+    String ABC = "Hello World! ";
+    System.out.println(ABC);
+    for (int i=2; i<=20; i+=2)
+      {
+        System.out.println(i);
+      }
+  }
+}
